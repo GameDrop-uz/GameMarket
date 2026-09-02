3909e002cae5718239f153003ece943b97b8c59e
GameDrop-uz <websayt15@gmail.com>
2026-09-02 20:57:26 +0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+const { Telegraf } = require('telegraf');
+
+// BotFather'dan olgan TOKENingizni quyidagi matn o'rniga yozasiz
+const bot = new Telegraf('8972752261:AAGH_-e0fU8wydNrzx2Ah5ihYgGl75AIZkQ');
+
+// Foydalanuvchi /start berganda ishlaydigan qism
+bot.start((ctx) => {
+    ctx.reply('Assalomu alaykum! Xush kelibsiz Game Market ga 🎮\n\nPastdagi tugmani bosing va ilovani ishga tushiring:', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    { 
+                        text: "🚀 Game Market'ni Ochish", 
+                        web_app: { url: "SIZNING_SAYTINGIZ_LINKI" } // Masalan: Vercel yoki Netlify'dagi link
+                    }
+                ]
+            ]
+        }
+    });
+});
+
+// Botni ishga tushirish
+bot.launch();
+console.log("Bot muvaffaqiyatli ishga tushdi!");
